87dc0c9b986e70601fd2f9f889a29a3f079937a5
skdlz <skdlzl753@naver.com>
2022-11-06 14:55:25 +0900

    석희

--- app/src/main/java/com/echo/echofarm/Activity/MainActivity.java
+++ app/src/main/java/com/echo/echofarm/Activity/MainActivity.java
@@ -29,7 +29,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     private NestedScrollView nestedSV;
     private Button searchBtn, morePostBtn;
     private ImageButton editPostBtn, chattingBtn, settingBtn;
-
+    //
     int count = 0;
 
     public MainActivity() {
